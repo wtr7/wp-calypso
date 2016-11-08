f51338270212735c0c731f2722e1ef48f041c2b1
James Tien <southp0105@gmail.com>
2016-11-08 15:17:51 +0800

    Move the tests for `isFetching` flag to `#account-recovery reducer action status flags` suite.

--- client/state/account-recovery/reducer.js
+++ client/state/account-recovery/reducer.js
@@ -6,65 +6,7 @@ import { combineReducers } from 'redux';
 /**
  * Internal dependencies
  */
-<<<<<<< HEAD
 import settings from './settings/reducer';
-=======
-import { createReducer } from 'state/utils';
-
-import {
-	ACCOUNT_RECOVERY_FETCH,
-	ACCOUNT_RECOVERY_FETCH_SUCCESS,
-	ACCOUNT_RECOVERY_FETCH_FAILED,
-
-	ACCOUNT_RECOVERY_PHONE_UPDATE,
-	ACCOUNT_RECOVERY_PHONE_UPDATE_SUCCESS,
-	ACCOUNT_RECOVERY_PHONE_UPDATE_FAILED,
-} from 'state/action-types';
-
-const createActionInProgressReducer = ( initiateActions, finishActions ) => {
-	const trueFunc = () => true;
-	const falseFunc = () => false;
-
-	const initiateHandlers = initiateActions.reduce(
-		( accumulator, actionType ) => ( { ...accumulator, [ actionType ]: trueFunc } ),
-		{}
-	);
-	const finishHandlers = finishActions.reduce(
-		( accumulator, actionType ) => ( { ...accumulator, [ actionType ]: falseFunc } ),
-		{}
-	);
-
-	return createReducer( false, {
-		...initiateHandlers,
-		...finishHandlers,
-	} );
-};
-
-const isFetching = createActionInProgressReducer(
-	[ ACCOUNT_RECOVERY_FETCH ],
-	[ ACCOUNT_RECOVERY_FETCH_SUCCESS, ACCOUNT_RECOVERY_FETCH_FAILED ]
-);
-
-const isUpdatingPhone = createActionInProgressReducer(
-	[ ACCOUNT_RECOVERY_PHONE_UPDATE ],
-	[ ACCOUNT_RECOVERY_PHONE_UPDATE_SUCCESS, ACCOUNT_RECOVERY_PHONE_UPDATE_FAILED ]
-);
-
-const data = createReducer( {}, {
-	[ ACCOUNT_RECOVERY_FETCH_SUCCESS ]: ( state, { email, email_validated, phone, phone_validated } ) => ( {
-		...state,
-		email,
-		emailValidated: email_validated,
-		phone,
-		phoneValidated: phone_validated
-	} ),
-
-	[ ACCOUNT_RECOVERY_PHONE_UPDATE_SUCCESS ]: ( state, { phone } ) => ( {
-		...state,
-		phone,
-	} ),
-} );
->>>>>>> 39ccb48... Add action handler tests for ACCOUNT_RECOVERY_PHONE_UPDATE,
 
 export default combineReducers( {
 	settings,
