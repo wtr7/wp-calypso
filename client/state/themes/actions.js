4b2746530924e88e76b80419e8e78bf81c32ed61
Bernhard Reiter <ockham@raz.or.at>
2016-11-18 22:42:47 +0100

    Add back old actions

--- client/state/themes/actions.js
+++ client/state/themes/actions.js
@@ -3,6 +3,7 @@
 /**
  * External dependencies
  */
+import { conforms, property } from 'lodash';
 import debugFactory from 'debug';
 
 /**
@@ -10,6 +11,15 @@ import debugFactory from 'debug';
  */
 import wpcom from 'lib/wp';
 import {
+	// Old action names
+	THEME_BACK_PATH_SET,
+	THEME_CLEAR_ACTIVATED,
+	THEME_DETAILS_RECEIVE,
+	THEME_DETAILS_RECEIVE_FAILURE,
+	THEME_DETAILS_REQUEST,
+	THEMES_INCREMENT_PAGE,
+	THEMES_QUERY,
+	// New action names
 	ACTIVE_THEME_REQUEST,
 	ACTIVE_THEME_REQUEST_SUCCESS,
 	ACTIVE_THEME_REQUEST_FAILURE,
@@ -30,9 +40,157 @@ import {
 	withAnalytics
 } from 'state/analytics/actions';
 import { getCurrentTheme } from './current-theme/selectors';
+import { isJetpackSite } from 'state/sites/selectors';
+import { getQueryParams } from './themes-list/selectors';
 import { getThemeById } from './themes/selectors';
 
 const debug = debugFactory( 'calypso:themes:actions' ); //eslint-disable-line no-unused-vars
+
+// Old actions
+
+export function fetchThemes( site ) {
+	return ( dispatch, getState ) => {
+		const queryParams = getQueryParams( getState() );
+		const startTime = new Date().getTime();
+
+		debug( 'Query params', queryParams );
+
+		return wpcom.undocumented().themes( site, queryParams )
+			.then( themes => {
+				const responseTime = ( new Date().getTime() ) - startTime;
+				return dispatch( legacyReceiveThemes( themes, site, queryParams, responseTime ) );
+			} )
+			.catch( error => receiveServerError( error ) );
+	};
+}
+
+export function fetchNextPage( site ) {
+	return dispatch => {
+		dispatch( incrementThemesPage( site ) );
+		return dispatch( fetchThemes( site ) );
+	};
+}
+
+export function query( params ) {
+	return {
+		type: THEMES_QUERY,
+		params: params
+	};
+}
+
+export function incrementThemesPage( site ) {
+	return {
+		type: THEMES_INCREMENT_PAGE,
+		site: site
+	};
+}
+
+export function fetchThemeDetails( id, site ) {
+	return dispatch => {
+		dispatch( {
+			type: THEME_DETAILS_REQUEST,
+			themeId: id
+		} );
+
+		wpcom.undocumented().themeDetails( id, site )
+			.then( themeDetails => {
+				debug( 'Received theme details', themeDetails );
+				dispatch( receiveThemeDetails( themeDetails ) );
+			} )
+			.catch( error => {
+				dispatch( receiveThemeDetailsFailure( id, error ) );
+			} );
+	};
+}
+
+export function receiveThemeDetails( theme ) {
+	return {
+		type: THEME_DETAILS_RECEIVE,
+		themeId: theme.id,
+		themeName: theme.name,
+		themeAuthor: theme.author,
+		themePrice: theme.price,
+		themeScreenshot: theme.screenshot,
+		themeScreenshots: theme.screenshots,
+		themeDescription: theme.description,
+		themeDescriptionLong: theme.description_long,
+		themeSupportDocumentation: theme.support_documentation || undefined,
+		themeDownload: theme.download_uri || undefined,
+		themeTaxonomies: theme.taxonomies,
+		themeStylesheet: theme.stylesheet,
+		themeDemoUri: theme.demo_uri
+	};
+}
+
+export function receiveThemeDetailsFailure( id, error ) {
+	debug( `Received error for theme ${ id }:`, error );
+	return {
+		type: THEME_DETAILS_RECEIVE_FAILURE,
+		themeId: id,
+		error: error,
+	};
+}
+
+export function receiveServerError( error ) {
+	return {
+		type: THEMES_RECEIVE_SERVER_ERROR,
+		error: error
+	};
+}
+
+const isFirstPageOfSearch = conforms( {
+	search: a => undefined !== a,
+	page: a => a === 1
+} );
+
+export function legacyReceiveThemes( data, site, queryParams, responseTime ) {
+	return ( dispatch, getState ) => {
+		const themeAction = {
+			type: THEMES_RECEIVE,
+			siteId: site.ID,
+			isJetpack: !! site.jetpack,
+			wasJetpack: isJetpackSite( getState(), site.ID ),
+			themes: data.themes,
+			found: data.found,
+			queryParams: queryParams
+		};
+
+		const trackShowcaseSearch = recordTracksEvent(
+			'calypso_themeshowcase_search',
+			{
+				search_term: queryParams.search || null,
+				tier: queryParams.tier,
+				response_time_in_ms: responseTime,
+				result_count: data.found,
+				results_first_page: data.themes.map( property( 'id' ) )
+			}
+		);
+
+		const action = isFirstPageOfSearch( queryParams )
+			? withAnalytics( trackShowcaseSearch, themeAction )
+			: themeAction;
+
+		dispatch( action );
+
+		return action;
+	};
+}
+
+export function clearActivated() {
+	return {
+		type: THEME_CLEAR_ACTIVATED
+	};
+}
+
+// Set destination for 'back' button on theme sheet
+export function setBackPath( path ) {
+	return {
+		type: THEME_BACK_PATH_SET,
+		path: path,
+	};
+}
+
+// New actions
 
 /**
  * Returns an action object to be used in signalling that a theme object has
@@ -180,13 +338,6 @@ export function requestActiveTheme( siteId ) {
 					error,
 				} );
 			} );
-	};
-}
-
-export function receiveServerError( error ) {
-	return {
-		type: THEMES_RECEIVE_SERVER_ERROR,
-		error: error
 	};
 }
 
