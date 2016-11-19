73221292fdd8329ba713b111ef02be02a87c726b
Copons <copons@users.noreply.github.com>
2016-11-19 18:46:41 -0300

    Fix wrong device button in SEO Preview
    On small screens, replaced the wrong phone preview button with the
    correct SEO preview one.

--- client/components/web-preview/toolbar.jsx
+++ client/components/web-preview/toolbar.jsx
@@ -81,15 +81,16 @@ export const PreviewToolbar = props => {
 					) ) }
 				</div>
 			}
-			{ showSEO &&
+			{ showDeviceSwitcher &&
 			<button
 				aria-hidden={ true }
 				key={ 'back-to-preview' }
 				className={ classNames(
 					'web-preview__device-button',
 					'web-preview__back-to-preview-button', {
-					'is-active': currentDevice !== 'seo'
-				} ) }
+						'is-active': currentDevice !== 'seo'
+					}
+				) }
 				onClick={ partial( setDeviceViewport, 'phone' ) }
 			>
 				<Gridicon icon="phone" />
@@ -99,11 +100,11 @@ export const PreviewToolbar = props => {
 				<button
 					aria-label={ translate( 'Show SEO and search previews' ) }
 					className={ classNames(
-						'web-preview__seo-button',
-						'web-preview__device-button', {
-						'is-active': 'seo' === currentDevice,
-						'is-showing-device-switcher': showDeviceSwitcher
-					} ) }
+						'web-preview__seo-button', {
+							'is-active': 'seo' === currentDevice,
+							'is-showing-device-switcher': showDeviceSwitcher
+						}
+					) }
 					onClick={ selectSeoPreview }
 				>
 					<Gridicon icon="globe" />
